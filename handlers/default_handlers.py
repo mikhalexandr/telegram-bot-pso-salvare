ff8e6b414366b2acff437c71b462f6061f83c8c3
Alexey <bazooka2008@inbox.ru>
2024-02-02 22:11:16 +0300

    aaa

--- handlers/default_handlers.py
+++ handlers/default_handlers.py
@@ -1,7 +1,14 @@
 from aiogram import Router
 from aiogram.types import Message
+from aiogram.filters import Command
+import consts
 
 router = Router()
+
+
+@router.message(Command("start"))
+async def start_handler(message: Message):
+    await message.answer(consts.START_MESSAGE)
 
 
 @router.message()
