f1f52397b53b1bb4fef62ed1a6e33877e38c9ef2
Alexey <bazooka2008@inbox.ru>
2024-02-02 23:21:05 +0300

    anketa

--- kb.py
+++ kb.py
@@ -5,3 +5,9 @@ def first_choose_kb():
     kb = [[KeyboardButton(text="Помогите найти!")], [KeyboardButton(text="Хочу помочь в поисках!")]]
     keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
     return keyboard
+
+
+def yes_or_no_kb():
+    kb = [[KeyboardButton(text="Да"), KeyboardButton(text="Нет")]]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    return keyboard
