36c7a3704d2944db6d4ffc2b54a7e8f0eecf8a54
Alexey <bazooka2008@inbox.ru>
2024-02-03 01:31:19 +0300

    kurator

--- handlers/tutor_handlers.py
+++ handlers/tutor_handlers.py
@@ -1,8 +1,8 @@
 from aiogram import Router, F, Bot
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
-import kb
-from states import LoadInfoStates
+from states import TutorStates
+import consts
 import db
 from filters import TutorFilter
 
@@ -11,17 +11,33 @@ router.message.filter(TutorFilter())
 
 
 @router.callback_query(F.data == "letsfind")
-async def accept_fin(callback: CallbackQuery, bot: Bot):
+async def accept_fin(callback: CallbackQuery, state: FSMContext, bot: Bot):
     info = db.get_checking_info()
-    await bot.send_message(info[0], "Ваш поиск одобрен!")
+    await state.set_state(TutorStates.descript_accept)
+    await state.update_data(user_id=info[0])
     db.delete_checking(info[1])
     db.push_lost_info(*info)
+    await bot.send_message(consts.TUTOR_ID, "Введите полную информацию про поиск:")
     await callback.answer()
 
 
 @router.callback_query(F.data == "reject")
-async def reject_fin(callback: CallbackQuery, bot: Bot):
+async def reject_fin(callback: CallbackQuery, state: FSMContext, bot: Bot):
     info = db.get_checking_info()
-    await bot.send_message(info[0], "К сожалению, ваш поиск отклонен!")
+    await state.set_state(TutorStates.descript_reject)
+    await state.update_data(user_id=info[0])
     db.delete_checking(info[1])
+    await bot.send_message(consts.TUTOR_ID, "Укажите причину отклонения запроса:")
     await callback.answer()
+
+
+@router.message(TutorStates.descript_reject, F.text)
+async def send_reject_msg(message: Message, state: FSMContext, bot: Bot):
+    await bot.send_message((await state.get_data())["user_id"], f"Ваш поиск отклонен!\n{message.text}")
+    await state.clear()
+
+
+@router.message(TutorStates.descript_accept, F.text)
+async def send_accept_msg(message: Message, state: FSMContext, bot: Bot):
+    await bot.send_message((await state.get_data())["user_id"], f"Ваш поиск принят!")
+    await state.clear()
