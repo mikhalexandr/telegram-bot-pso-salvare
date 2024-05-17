1e0ce972ae688aeb5356f709563d6da86437e590
Alexandr <mikhalchenkov22@gmail.com>
2024-05-17 21:49:20 +0300

    ✨feat: command that shows form template

--- handlers/default_handlers.py
+++ handlers/default_handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
@@ -14,6 +14,12 @@ router = Router()
 async def start_handler(message: Message, state: FSMContext):
     await message.answer(consts.START_MESSAGE)
     await state.set_state(LoadingNameStates.load_name)
+
+
+@router.message(Command("form_template"))
+async def form_template_handler(message: Message):
+    await message.answer_photo(photo=FSInputFile(path="data/form-template.png"),
+                               caption="Шаблон анкеты о пострадавшем")
 
 
 @router.message()
